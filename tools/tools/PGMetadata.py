757c830a0ad7255b942ca843ac4e1c187bb71a08
Andrea Borghi <andrea.borghi@camptocamp.com>
2023-11-09 14:01:45 +0100

    wip

--- tools/tools/PGMetadata.py
+++ tools/tools/PGMetadata.py
@@ -30,8 +30,6 @@ class PGMetadata:
         # Create a cursor object using the connection
         cur = self.conn.cursor()
 
-        # Use the psycopg2.sql.SQL class to safely format the SQL query
-        # query = sql.SQL("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'Verbindungspunkte' and table_schema = 'detailnetz'").format(
         query = sql.SQL(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name =  '{table_name}'  and table_schema = '{schema_name}'")
 
         # Execute the query
@@ -53,10 +51,14 @@ class PGMetadata:
         query_geo = sql.SQL(f"SELECT f_geometry_column,type,srid FROM geometry_columns WHERE f_table_schema = '{schema_name}' AND f_table_name = '{table_name}'")
         cur.execute(query_geo)
         rows_geo = cur.fetchall()
+        
         log.debug(f'rows_geo = {rows_geo}')
+        
+        # explicit override the geometry column
         geom_col = rows_geo[0][0]
         geom_type = rows_geo[0][1]
         srid = rows_geo[0][2]
+        
         self.metadata[geom_col]={"type": geom_type, "srid": srid}
         log.debug(f"self.metadata = {self.metadata}")
         # Close the cursor and connection
